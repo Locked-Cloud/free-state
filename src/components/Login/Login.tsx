8023ada1c18d09f0aa5363159b217bbea29835c4
Ibrahim Ahmed <142117977+Locked-Cloud@users.noreply.github.com>
2025-07-21 20:49:21 +0300

    25

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { useNavigate, useLocation } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import styles from "./Login.module.css";
 import { useAuth } from "../../contexts/AuthContext";
 
