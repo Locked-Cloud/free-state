8653c9c50eff93b03f894aeac314ad0663398b37
Ibrahim Ahmed <142117977+Locked-Cloud@users.noreply.github.com>
2025-07-21 21:19:01 +0300

    simple

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -30,7 +30,7 @@ const Navbar: React.FC = () => {
   return (
     <nav className={styles.navbar}>
       <Link to="/" className={styles.logo} onClick={closeMenu}>
-        Free State
+        Free
       </Link>
 
       {isAuthenticated && (
