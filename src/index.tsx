a1aa2e90504bc39b3e87633ac4c9eff657b5ae47
Ibrahim Ahmed <142117977+Locked-Cloud@users.noreply.github.com>
2025-08-20 13:11:25 +0300

    back

--- src/index.tsx
+++ src/index.tsx
@@ -7,7 +7,11 @@ import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
-
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
